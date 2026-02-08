1d2e489f4bc54b2afd97120459eff757c2d4963f
dipanshu-vivobook <dipanshu.vivobook@gmail.com>
2026-02-08 18:37:57 +0530

    1st class

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,31 @@
+/*
+?=========================================================================================================
+?----------------------- Let , Var, Const execution for the javascript -----------------------------------
+?=========================================================================================================
+*/
+
+const accountId = 123432;
+let accountEmail = "demo@gmail.com";
+var accountPassword = "password";
+accountCity = "delhi";
+
+// let accountState;
+var accountState;
+
+// accountId = 12345;
+accountEmail = "dipanshu@gmail.com";
+accountPassword = "dipanshu";
+accountCity = "mumbai";
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
