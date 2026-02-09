23fde34d50dbc7bf25d9d62497d13683d6f8056e
dipanshu-vivobook <dipanshu.vivobook@gmail.com>
2026-02-09 22:29:51 +0530

    add datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // Treact all Js code as newer version
+
+// alert("hello"); // we are using node js not browser
+
+/*
+ *============== DataTypes of Javscript ====================
+ */
+
+//?------------ Primitive Datatypes ------------------
+
+let name = "Dipanshu"; // String
+let age = 21; // Number
+let isLoogedIn = true || false; // Boolean;
+let city = null; // Null ( Sandalone value )
+let state; // Undefined ( assign but not assigned the value )
+// Symbol => Unique
+
+//?----------- Non Primitive datatypes---------------
+
+//! (1) Object
+
+console.log(typeof city); // Object
+console.log(typeof state);
