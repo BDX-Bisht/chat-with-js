fc1467143803f131bfb2aa7e317ddffd8fda1fb1
dipanshu-vivobook <dipanshu.vivobook@gmail.com>
2026-02-09 22:45:10 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,44 @@
+// let score = "33we";
+// let score = undefined;
+// let score = false;
+let score = true;
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//* "33" => 33
+//* "33asd" => NaN
+//* true => 1 , false => 0
+
+// let isLoogedIn = 1;
+// let isLoogedIn = 0;
+// let isLoogedIn = "";
+// let isLoogedIn = "as";
+let isLoogedIn = 0;
+
+let booleanIsLoggedIn = Boolean(isLoogedIn);
+
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+//* 1 => true
+//* 0 => false
+//* "" => false
+//* "dipanshu" => true
+
+// let someNumber = 33;
+let someNumber = true;
+
+let stringNumber = String(someNumber);
+
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//* 33 => "33"
+//* true => "true"
+
+//!===============
