64fbe9b7dff435b60919155bdc20e3f2bef0dbc4
dipanshu-vivobook <dipanshu.vivobook@gmail.com>
2026-03-07 23:16:47 +0530

    refactor: comment out console logs and enhance function structure

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,9 +5,35 @@ if (true) {
     let a = 10;
     const b = 20;
     // var c = 30;
-    console.log(a);
+    // console.log(a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one() {
+    const username = "hitesh";
+    function two() {
+        const website = "bdxBisht";
+        console.log(username);
+    }
+    // console.log(website);
+    two();
+}
+
+// one();
+
+//!+++++++++++++++ Interesting ++++++++++++
+
+console.log(addOne(5));
+function addOne(num) {
+    return num + 1;
+}
+
+// console.log(addTwo(5)); //?---- This is not work due to hoisting
+const addTwo = function (num) {
+    return num + 2;
+};
+
+console.log(addTwo(5));
