e764da94edbc18a141d9a68260d2c2c573a62ddd
dipanshu-vivobook <dipanshu.vivobook@gmail.com>
2026-03-14 23:00:55 +0530

    feat: add JavaScript Execution Context documentation

---
+++ 03_basics/05_execution_context.js
@@ -0,0 +1,11 @@
+//*****   Javascript Execution Context   ****/
+
+//? 1) GLobal Execution Context used as ( this )
+
+//? 2) Function (Functional) Execution Context
+
+//? 3) Eval Execution Context
+
+
+//? Memory Creation Phase
+//? Execution Phase
