cbce2d32e152ae1b049307fc32c661135f323add
dipanshu-vivobook <dipanshu.vivobook@gmail.com>
2026-03-17 23:39:12 +0530

    feat: add while and do while loop examples

---
+++ 05_iterations/two.js
@@ -0,0 +1,25 @@
+//!---------- while loop ------------
+
+let index = 0;
+
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2;
+// }
+
+let myArr = ["flash", "superman", "batman"];
+let arr = 0;
+
+while (arr < myArr.length) {
+    // console.log(`Superhero is ${myArr[arr]}.`);
+    arr = arr + 1;
+}
+
+//!-------- do while loop--------------
+
+let score = 11;
+
+do {
+    console.log(`Score is ${score}`);
+    score = score + 1;
+} while (score <= 10);
